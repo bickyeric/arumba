36ffbcf7320c6e47acf71d0d093ea147bc7f81c9
Bicky Eric Kantona <bickyeric@ymail.com>
2018-10-09 01:34:49 +0700

    tambah handler /start

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"encoding/base64"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
+
+func handleStartCommand(message *tgbotapi.Message) {
+	arg := message.CommandArguments()
+	if arg == "" {
+		return
+	}
+
+	decodedArg, err := base64.StdEncoding.DecodeString(arg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	decodedString := string(decodedArg)
+	splittedString := strings.Split(decodedString, "_")
+
+	resp, _ := http.Get("https://backend-bot.000webhostapp.com/index.php/comic/read/userid/" + splittedString[0] + "/" + splittedString[1])
+	json, _ := ioutil.ReadAll(resp.Body)
+	log.Printf("%s\n", json)
+	resp.Body.Close()
+}
 
 func main() {
 
@@ -21,14 +43,17 @@ func main() {
 		log.Fatal(err)
 	}
 
-	bot.Debug = true
+	bot.Debug = false
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	updates := bot.ListenForWebhook("/" + bot.Token)
 	go http.ListenAndServe("0.0.0.0:"+port, nil)
 
 	for update := range updates {
-		log.Print("%+v\n", update)
+		command := update.Message.Command()
+		if command == "start" {
+			handleStartCommand(update.Message)
+		}
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Masih Development mang!!!")
 		bot.Send(msg)
 	}
