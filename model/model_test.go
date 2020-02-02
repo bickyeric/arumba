269d979c7d3f32c208c65e3006c2c666f20585e9
Bicky Eric Kantona <bickyeric@ymail.com>
2020-02-02 19:04:12 +0700

    timestamp marshaller test

--- model/model_test.go
+++ model/model_test.go
@@ -2,7 +2,9 @@ package model_test
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/bickyeric/arumba/model"
 	"github.com/stretchr/testify/suite"
@@ -11,6 +13,30 @@ import (
 
 type modelSuite struct {
 	suite.Suite
+}
+
+func (s *modelSuite) TestUnmarshalTimestamp() {
+	expected := time.Now()
+	actual, err := model.UnmarshalTimestamp(expected.Unix())
+	s.Nil(err)
+	s.Equal(expected.Year(), actual.Year())
+	s.Equal(expected.Month(), actual.Month())
+	s.Equal(expected.Day(), actual.Day())
+	s.Equal(expected.Hour(), actual.Hour())
+	s.Equal(expected.Minute(), actual.Minute())
+	s.Equal(expected.Second(), actual.Second())
+
+	_, err = model.UnmarshalTimestamp("invalid id")
+	s.NotNil(err)
+}
+
+func (s *modelSuite) TestMarshalTimestamp() {
+	var b bytes.Buffer
+	expected := time.Now()
+
+	m := model.MarshalTimestamp(expected)
+	m.MarshalGQL(&b)
+	s.Equal(fmt.Sprintf("%d", expected.Unix()), b.String())
 }
 
 func (s *modelSuite) TestUnmarshalID() {
