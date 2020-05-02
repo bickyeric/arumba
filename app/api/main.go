68da2dc7f4e04854a28e80a79ed11fdecea72811
Bicky Eric Kantona <bickyeric@ymail.com>
2020-05-03 00:20:21 +0700

    secret target path

--- app/api/main.go
+++ app/api/main.go
@@ -21,7 +21,7 @@ import (
 )
 
 func main() {
-	gotenv.Load(".env", "/run/secrets/arumba-api")
+	gotenv.Load(".env", "/run/secrets/env-file")
 	ctx := context.Background()
 
 	// region    ************************** CONNECTION **************************
